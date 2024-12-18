dc32ec2ef4a4f5529579303e37db73bfed0e46c2
Matthew Heidelbaugh <matt9hsix@gmail.com>
2024-12-18 15:55:20 -0500

    aws kendra implementation

--- c7n/resources/kendra.py
+++ c7n/resources/kendra.py
@@ -1,8 +1,16 @@
 from c7n.actions import Action
 from c7n.filters.kms import KmsRelatedFilter
 from c7n.manager import resources
-from c7n.query import QueryResourceManager, TypeInfo
+from c7n.query import DescribeSource, QueryResourceManager, TypeInfo
+from c7n.tags import universal_augment
 from c7n.utils import local_session, type_schema
+
+
+class DescribeIndex(DescribeSource):
+
+    def augment(self, resources):
+        return universal_augment(self.manager, super().augment(resources))
+
 
 @resources.register('kendra')
 class KendraIndex(QueryResourceManager):
@@ -12,19 +20,23 @@ class KendraIndex(QueryResourceManager):
         arn_type = 'index'
         enum_spec = ('list_indices', 'IndexConfigurationSummaryItems', None)
         detail_spec = (
-            'describe_index', 'Name', 'Name', None)
+            'describe_index', 'Id', 'Id', None)
         id = 'Id'
         name = 'Name'
         date = 'UpdatedAt'
         universal_taggable = object()
         cfn_type = config_type = 'AWS::Kendra::Index'
 
+    source_mapping = {
+        'describe': DescribeIndex,
+    }
+
 
 @KendraIndex.filter_registry.register('kms-key')
 class IndexKmsFilter(KmsRelatedFilter):
 
     RelatedIdsExpression = 'ServerSideEncryptionConfiguration.KmsKeyId'
-    
+
 
 @KendraIndex.action_registry.register('delete')
 class IndexDelete(Action):
