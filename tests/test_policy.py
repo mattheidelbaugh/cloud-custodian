78914810ecde937ecdffe9a0d0c961571205004a
Matthew Heidelbaugh <matt9hsix@gmail.com>
2025-01-06 17:20:03 -0500

    exempt from test_resource_type_empty_metadata

--- tests/test_policy.py
+++ tests/test_policy.py
@@ -733,7 +733,7 @@ class PolicyMetaLint(BaseTest):
         empty = set()
         for k, v in manager.resources.items():
             if k in ('rest-account', 'account', 'codedeploy-deployment', 'sagemaker-cluster',
-                     'networkmanager-core'):
+                     'networkmanager-core', 'quicksight-account'):
                 continue
             for rk, rv in v.resource_type.__dict__.items():
                 if rk[0].isalnum() and rv is None:
