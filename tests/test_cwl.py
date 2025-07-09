63d2f60c4d1f3bb5c153f19714e953df09ced518
Matthew <matt9hsix@gmail.com>
2025-07-09 14:13:44 -0400

    tests

--- tests/test_cwl.py
+++ tests/test_cwl.py
@@ -577,7 +577,7 @@ class DestinationTest(BaseTest):
 
 
 class DeliveryDestinationTest(BaseTest):
-    def test_log_destination(self):
+    def test_delivery_destination(self):
         factory = self.replay_flight_data('test_delivery_destination')
         client = local_session(factory).client('logs')
         p = self.load_policy(
@@ -622,7 +622,7 @@ class DeliveryDestinationTest(BaseTest):
         tags = client.list_tags_for_resource(resourceArn=resources[0]['arn'])['tags']
         assert 'test-tag' not in tags
 
-    def test_log_destination_cross_account(self):
+    def test_delivery_destination_cross_account(self):
         factory = self.replay_flight_data('test_delivery_destination_cross_account')
         p = self.load_policy(
             {
@@ -636,7 +636,7 @@ class DeliveryDestinationTest(BaseTest):
         resources = p.run()
         self.assertEqual(len(resources), 1)
 
-    def test_log_destination_cross_account_no_policy(self):
+    def test_delivery_destination_cross_account_no_policy(self):
         factory = self.replay_flight_data('test_delivery_destination_cross_account_no_policy')
         p = self.load_policy(
             {
@@ -650,7 +650,7 @@ class DeliveryDestinationTest(BaseTest):
         resources = p.run()
         self.assertEqual(len(resources), 0)
 
-    def test_log_destination_delete(self):
+    def test_delivery_destination_delete(self):
         factory = self.replay_flight_data('test_delivery_destination_delete')
         client = local_session(factory).client('logs')
         p = self.load_policy(
